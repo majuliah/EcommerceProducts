74ec78d94fa9b9b859edbe704b70131c2a19b170
Majulia <majulialcarvalho@gmail.com>
2021-11-20 11:45:08 -0300

    estabelecida a conexão com o banco de dados

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -13,6 +13,8 @@ namespace MVCapp.Models
     public class Categoria
     {
         public int Id { get; set; }
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "Campo não pode ser vazio")]
         public string Descricao { get; set; }   
 
         public List<Produto> Produtos { get; set; }
--- Models/Produto.cs
+++ Models/Produto.cs
@@ -1,9 +1,18 @@
-﻿namespace MVCapp.Models
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace MVCapp.Models
 {
     public class Produto
     {
         public int Id { get; set; }
+        [Display(Name = "Descrição")]
         public string Descricao  { get; set; }
+        [Range(1, 10, ErrorMessage = "Valor fora da faixa")]
         public int Quantidade { get; set; }
 
         public int CategoriaId { get; set; }   
